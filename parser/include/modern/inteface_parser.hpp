765f8cb556d8f4dc3abe274144533d2bf906db95
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-02 09:20:14 -0500

    Added common interface
    - Abstract class to handle common functions to parse a cloud file
    - This class has introduced the load_cloudfile function to be implemented in derivated classes

--- parser/include/modern/inteface_parser.hpp
+++ parser/include/modern/inteface_parser.hpp
@@ -1,4 +1,9 @@
 #pragma once
+#include <pcl/console/print.h>
+#include <pcl/console/time.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
 #include <iostream>
 #include <map>
 #include <string>
@@ -7,7 +12,7 @@ namespace CloudParserLibrary {
 
 class InterfaceParser {
  public:
-  virtual void load_cloudfile(std::string filename) {}
+  virtual void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {}
 };
 
 }  // namespace CloudParserLibrary
