1d17059feb0b51dc3d30390d2fee74e94ecafe1f
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-02 09:21:42 -0500

    Parser factory
    - Class to create/register new parser formats using factory design pattern
    - Defines a ParserCloudFile class to be used as a generic cloud factory given an input file

--- parser/include/modern/parser.hpp
+++ parser/include/modern/parser.hpp
@@ -1,17 +1,35 @@
 #pragma once
 #include "concrete_parses.hpp"
-
 namespace CloudParserLibrary {
 
 class ParserFactory {
  public:
-  ParserFactory() { initializes_factories(); }
-  void initializes_factories();
+  ParserFactory() { ParserFactory::register_format("PCD", new ParserPCD()); }
   void register_format(std::string format, InterfaceParser *ptr);
   InterfaceParser *get_parser(const std::string format);
 
  private:
   std::map<std::string, InterfaceParser *> factories = {};
+};
+
+class ParserCloudFile {
+ public:
+  ParserFactory parser_factory;
+  void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr &cloud) {
+    int position = filename.find_last_of(".");
+    std::string extension = filename.substr(position + 1);
+    std::transform(extension.begin(), extension.end(), extension.begin(), ::toupper);
+    InterfaceParser *cloudparser = parser_factory.get_parser(extension);
+    pcl::console::TicToc tt;
+    pcl::console::print_highlight("Loading ");
+    cloudparser->load_cloudfile(filename, cloud);
+    pcl::console::print_info("\nFound pcd file.\n");
+    pcl::console::print_info("[done, ");
+    pcl::console::print_value("%g", tt.toc());
+    pcl::console::print_info(" ms : ");
+    pcl::console::print_value("%d", cloud->size());
+    pcl::console::print_info(" points]\n");
+  }
 };
 
 }  // namespace CloudParserLibrary
