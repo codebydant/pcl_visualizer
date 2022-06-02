253e53ca950e95b5c39434d96216dfb37d0b38bf
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-02 09:18:51 -0500

    Added concrete interface
    - This commit has introduced the parser/include/modern/concrete_parses.hpp
    - This header has the Parses definitions for each supported type (.PCD at the moment)

--- parser/include/modern/concrete_parses.hpp
+++ parser/include/modern/concrete_parses.hpp
@@ -1,19 +1,16 @@
 #pragma once
+#include <pcl/io/pcd_io.h>
+
 #include "inteface_parser.hpp"
 namespace CloudParserLibrary {
 class ParserPCD : public InterfaceParser {
-public:
+ public:
   std::string parser_name = "ParserPCD";
-  void load_cloudfile(std::string filename) {
-    std::cout << "pcdparser implementation" << std::endl;
+  void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
+    if (pcl::io::loadPCDFile(filename, *cloud) < 0) {
+      pcl::console::print_error("Error loading point cloud %s \n", filename.c_str());
+      std::exit(-1);
+    }
   }
 };
-
-class ParserPLY : public InterfaceParser {
-public:
-  std::string parser_name = "ParserPLY";
-  void load_cloudfile(std::string filename) {
-    std::cout << "plyparser implementation" << std::endl;
-  }
-};
-} // namespace CloudParserLibrary
+}  // namespace CloudParserLibrary
