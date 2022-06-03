0da60bc5036db4b417352121dbbb2119e6d4d782
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-03 08:01:45 -0500

    added module documentation

--- parser/include/modern/inteface_parser.hpp
+++ parser/include/modern/inteface_parser.hpp
@@ -1,4 +1,18 @@
+/*
+ * Abstract class for interface parses
+ *
+ * it implements an abstract class to be used by concrete classes. This module will
+ * a common blueprint to the parser format.
+ *
+ * load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) must be
+ * defined in the derived class to implement the new parser format extension.
+ *
+ * Be aware that this interface is being used in a factory class to provide supported parser
+ * extensions to the client and is not meant to be used as an instance object
+ */
 #pragma once
+#ifndef INTERFACE_PARSER_HPP
+#define INTERFACE_PARSER_HPP
 #include <pcl/console/print.h>
 #include <pcl/console/time.h>
 #include <pcl/point_cloud.h>
@@ -12,7 +26,8 @@ namespace CloudParserLibrary {
 
 class InterfaceParser {
  public:
-  virtual void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {}
+  virtual void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) = 0;
 };
 
 }  // namespace CloudParserLibrary
+#endif
