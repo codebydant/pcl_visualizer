649522e4a73812810324cc5f604c086156b9d05d
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-03 08:00:38 -0500

    register ply format

--- parser/include/modern/parser.hpp
+++ parser/include/modern/parser.hpp
@@ -1,10 +1,30 @@
+/*
+ * Implementation of a parser factory for cloud parses
+ *
+ * it implements a factory design pattern to create a parser from a concrete class.
+ * each concrete parser is derived from an abstract class InterfaceParser and this
+ * module provides a ParserFactory class to register new formats.
+ *
+ * ParserCloudFile is the common interface to load a cloud file given a path. This
+ * interface will check if the current file extension is supported by the factories list.
+ * In case there is a provided format, a parser object extension will be provided to the client.
+ *
+ * Current supported formats are: [.pcd, .ply]
+ */
+
 #pragma once
+#ifndef PARSER_HPP
+#define PARSER_HPP
 #include "concrete_parses.hpp"
+
 namespace CloudParserLibrary {
 
 class ParserFactory {
  public:
-  ParserFactory() { ParserFactory::register_format("PCD", new ParserPCD()); }
+  ParserFactory() {
+    ParserFactory::register_format("PCD", new ParserPCD());
+    ParserFactory::register_format("PLY", new ParserPLY());
+  }
   void register_format(std::string format, InterfaceParser *ptr);
   InterfaceParser *get_parser(const std::string format);
 
@@ -23,7 +43,7 @@ class ParserCloudFile {
     pcl::console::TicToc tt;
     pcl::console::print_highlight("Loading ");
     cloudparser->load_cloudfile(filename, cloud);
-    pcl::console::print_info("\nFound pcd file.\n");
+    pcl::console::print_info("\nFound %s file.\n", extension.c_str());
     pcl::console::print_info("[done, ");
     pcl::console::print_value("%g", tt.toc());
     pcl::console::print_info(" ms : ");
@@ -33,3 +53,4 @@ class ParserCloudFile {
 };
 
 }  // namespace CloudParserLibrary
+#endif
