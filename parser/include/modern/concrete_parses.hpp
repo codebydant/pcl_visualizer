0b2e015a0f28df3cb57728a271e3b5d900290cc0
Daniel Tobon <daniel.tobon@uao.edu.co>
2022-06-03 08:01:20 -0500

    register ply format

--- parser/include/modern/concrete_parses.hpp
+++ parser/include/modern/concrete_parses.hpp
@@ -1,7 +1,19 @@
+/*
+ * Concrete parsers module
+ *
+ * it implements a factory to create a parser from a abstract class.
+ * This module will provided support for new format extensions.
+ */
 #pragma once
+#ifndef CONCRETE_PARSES_HPP
+#define CONCRETE_PARSES_HPP
 #include <pcl/io/pcd_io.h>
+#include <pcl/io/ply/ply.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/vtk_lib_io.h>
 
 #include "inteface_parser.hpp"
+
 namespace CloudParserLibrary {
 class ParserPCD : public InterfaceParser {
  public:
@@ -13,4 +25,42 @@ class ParserPCD : public InterfaceParser {
     }
   }
 };
+
+class ParserPLY : public InterfaceParser {
+ public:
+  std::string parser_name = "ParserPLY";
+  bool cloud_is_good(pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
+    if ((cloud->points.size() > 0) or
+        (cloud->points[0].x > 0 && cloud->points[0].y > 0 && cloud->points[0].z > 0)) {
+      return true;
+    }
+    return false;
+  }
+  void load_cloudfile(std::string filename, pcl::PointCloud<pcl::PointXYZRGB>::Ptr& cloud) {
+    pcl::io::loadPLYFile(filename, *cloud);
+    if (cloud_is_good(cloud)) {
+      return;
+    }
+
+    pcl::console::print_warn("\nloadPLYFile could not read the cloud, attempting to loadPolygonFile...\n");
+
+    pcl::PolygonMesh cl;
+    pcl::io::loadPolygonFile(filename, cl);
+    pcl::fromPCLPointCloud2(cl.cloud, *cloud);
+    if (cloud_is_good(cloud)) {
+      return;
+    }
+    pcl::console::print_warn("\nloadPolygonFile could not read the cloud, attempting to PLYReader...\n");
+
+    pcl::PLYReader plyRead;
+    plyRead.read(filename, *cloud);
+    if (cloud_is_good(cloud)) {
+      return;
+    }
+
+    pcl::console::print_error("\nError .ply file is not compatible.\n");
+    std::exit(-1);
+  }
+};
 }  // namespace CloudParserLibrary
+#endif
